c57be6b9ba2da4349d8d685c562d5ffa72bcac71
Ricardo Han <ricardoshhan@gmail.com>
2017-12-26 13:49:56 -0200

    first website

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function navScroll() {
+  var body = document.body;
+  var element = document.documentElement;
+  var nav = document.querySelector('.navbar');
+
+  if(body.scrollTop > 180 || element.scrollTop > 180 /*|| window.pageYOffset > 100 || window.scrollY > 100*/) {
+    nav.classList.add('navbar_back');  // className é mais compatível (IE 8 e 9), classList é mais moderno
+  } else {
+    nav.classList.remove('navbar_back');
+  }
+};
+
+window.onscroll = function() {
+  navScroll();
+  // fadeInElements();
+  // scrollPosition(); ver a posição y no console
+};
+
+// show galery elements -----
+var boxes = document.querySelectorAll('.hide');
+var button = document.querySelector('.button');
+
+button.addEventListener('click', removeClass);
+
+function removeClass() {
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].classList.remove('hide');
+  }
+  button.classList.add('hide');
+}
+// --------
